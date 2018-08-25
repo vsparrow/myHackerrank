f0bd535224895a1b4e8d35d044808ae9d4f1dafc
MKHAN <khan@vsparrow.com>
2018-08-24 23:38:13 -0400

    working solution

--- algo.js
+++ algo.js
@@ -22,10 +22,15 @@
 // It doesn't matter what values are set beyond the returned length.
 
 var removeElement = function(nums, val) {
-
+  while(nums.indexOf(val) != -1){
+    let index  = nums.indexOf(val)
+    nums.splice(index,1)
+  }
+  return nums.length
 };
 
-
+console.log(removeElement([3,2,2,3],3));
+console.log(removeElement([0,1,2,2,3,0,4,2],2));
 
 //The count-and-say sequence is the sequence of integers with the first five terms as following:
 //
