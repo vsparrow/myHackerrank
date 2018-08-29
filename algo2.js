496a641997920fa111c44fbb3a1acda20775b2a4
MKHAN <khan@vsparrow.com>
2018-08-28 23:21:35 -0400

    comment out function subset for redo

--- algo2.js
+++ algo2.js
@@ -1,37 +1,42 @@
-// Given a set of distinct integers, nums, return all possible subsets (the power set).
 //
-// Note: The solution set must not contain duplicate subsets.
+// //not sure will have to researc hand come back to this
 //
-// Example:
+// // Given a set of distinct integers, nums, return all possible subsets (the power set).
+// //
+// // Note: The solution set must not contain duplicate subsets.
+// //
+// // Example:
+// //
+// // Input: nums = [1,2,3]
+// // Output:
+// // [
+// //   [3],
+// //   [1],
+// //   [2],
+// //   [1,2,3],
+// //   [1,3],
+// //   [2,3],
+// //   [1,2],
+// //   []
+// // ]
 //
-// Input: nums = [1,2,3]
-// Output:
-// [
-//   [3],
-//   [1],
-//   [2],
-//   [1,2,3],
-//   [1,3],
-//   [2,3],
-//   [1,2],
-//   []
-// ]
-let subset = (nums)=>{
-  let subset = []
-  //get all single digits
-  nums.forEach((element)=>{subset.push([element])})
-  while(nums.length > 0){
-    //loop through each element combo, shifting of first element  in each round
-    let miniset = []
-    for(let i=0;i< nums.length;i++){
-      miniset.push(nums[i])
-      subset.push(miniset)
-    }//for
-    nums.shift()
-  }//while
-
-  return subset
-}//function
-
-subset([1,2,3])
-// console.log(subset([1,2,3]));
+//   let subset = (nums)=>{
+//     let subset = []
+//     //get all single digits
+//     // nums.forEach((element)=>{subset.push([element])})
+//     while(nums.length > 0){
+//       //loop through each element combo, shifting of first element  in each round
+//       let miniset = []
+//       for(let i=0;i< nums.length;i++){
+//         miniset.push(nums[i])
+//         console.log(miniset)
+//         subset.push(miniset)
+//       }//for
+//       nums.shift()
+//     }//while
+//
+//     return subset
+//   }//function
+//
+// subset([1,2,3])
+// // console.log(subset([1,2,3]));
