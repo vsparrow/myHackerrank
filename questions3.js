0248fa890131af6b4a589aa04c18ee7e77937248
M Khan <khan@vsparrow.com>
2018-10-10 08:44:12 -0400

    Add function can self divide

--- questions3.js
+++ questions3.js
@@ -1,7 +1,24 @@
 
+let canSelfDivide = (num)=>{
+	let numToString = num.toString().split("")
+	numToString.forEach((n)=>{
+		if((num % n ) != 0)
+		{
+			return false
+		}
+	})
+	return true
+	return num
+}
 
 var selfDividingNumbers = function(left, right) {
-    
+    let selfDividing = []
+    for(let i = left; i <= right; i++)
+    {
+    	console.log(canSelfDivide(i))
+    }
+
+    return "test"
 };
 
 // A self-dividing number is a number that is divisible by every digit it contains.
@@ -15,7 +32,8 @@ var selfDividingNumbers = function(left, right) {
 
 // Example 1:
 // Input: 
-// left = 1, right = 22
+let left = 1, right = 22
+console.log(selfDividingNumbers(left,right))
 // Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22]
 // Note:
 
