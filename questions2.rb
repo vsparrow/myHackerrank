37f1422dbabe7a1cda9fa7b704f004471cb88828
MKHAN <khan@vsparrow.com>
2018-11-28 21:29:23 -0500

    return ll passed test

--- questions2.rb
+++ questions2.rb
@@ -31,14 +31,18 @@ def middle_node(head) #input from leet code coming in as array so this is an arr
 end
 #***********************************************TESTS
 input = [1,2,3,4,5]       #input from leet code coming in as array
-puts middle_node(input)
+ll = middle_node(input)
+#TEST
+puts ll.next.next.value #should be 5
 # Output: Node 3 from this list (Serialization: [3,4,5])
 # The returned node has value 3.  (The judge's serialization of this node is [3,4,5]).
 # Note that we returned a ListNode object ans, such that:
 # ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, and ans.next.next.next = NULL.
 
 input =  [1,2,3,4,5,6]
-puts middle_node(input)
+ll = middle_node(input)
+#TEST
+puts ll.next.next.value #should be 6
 
 # Output: Node 4 from this list (Serialization: [4,5,6])
 # Since the list has two middle nodes with values 3 and 4, we return the second one.
